033be192f67dd143bd62360504a8cedcfb03783b
JanetZheng516 <janet@pop-os.localdomain>
2021-01-11 22:03:25 +0100

    menu

---
+++ main.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+var cardMenuItems = document.getElementsByClassName('card-menu');
+for (const item of cardMenuItems) {
+  item.addEventListener('click', () => {
+    if(!item.className.includes('card-menu--active')){
+      item.classList.add('card-menu--active')
+    } else {
+      item.classList.remove('card-menu--active')
+    }
+  });
+}
